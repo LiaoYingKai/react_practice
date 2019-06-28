d9b769b1666348d0fb318ad8fa6c0cffd7476864
Kai <b10223052@yuntech.org.tw>
2019-06-28 16:10:38 +0800

    modify inputBox style with input and button

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 let TodoInput = (props) =>(
-  <div>
-    <input type="text" onChange={props.onChange} value={props.text} />
+  <div className={"todoInputBox"}>
+    <div className="inputContainer">
+      <input id="input" type="text" onChange={props.onChange} value={props.text} placeholder="請輸入內容"/>
+      <label htmlFor="input">請輸入內容</label>
+      <div className="bottom-line"></div>
+    </div>
     <button onClick={props.submitTodo}>Add Item</button>
   </div>
 )
